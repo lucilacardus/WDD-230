2c70dac77585d7006f38719154cf0f2ad8014911
lucilacardus <lucila.cardus@gmail.com>
2019-06-29 18:25:28 -0300

    5465

--- js/w10.1.js
+++ js/w10.1.js
@@ -1,25 +1,26 @@
 /* Defining Table
-Input: Get data from Table
-Processing: Run a loop to discover which is the oldest temple
-Output: Display oldest temple for the user to see.*/
+Input: Get current temperature and current wind speed  in Fahrenheit from the user/
+Processing: Determine IF we can define the wind chill factor based upon the maximum temperature constraint (50°F) and minimum wind speed constraint (3 mph). If the input parameters do not meet those constraints, then the function should return "N/A" which means "Not Applicable".
+Output: the wind chill factor in Fahrenheit or NA if apliable */
 
+function doInputOutput() {
+	//input
+	let temperature = parseFloat(document.getElementById("temperature").value);
+	let windSpeed = parseFloat(document.getElementById("speed").value);
 
-//Input
-let table = document.getElementById("templeData");
-let ayear = parseInt(table.rows[1].cells[1].innerHTML);
-let temple = table.rows[1].cells[0].innerHTML;
-console.table(table)
+	let answer = windChill(temperature, windSpeed);
 
-parseInt(table.rows[1].cells[1].innerHTML);
-//Processing
-for (let i = 1; i < table.rows.length; i++) { 
-let year = parseInt(table.rows[i].cells[1].innerHTML);
-if (ayear > year) {
-ayear = year
-temple = table.rows[i].cells[0].innerHTML;
-}
+	document.getElementById("output").innerHTML = answer;
 }
 
-
-//Output
-document.getElementById("output").innerHTML = temple;
+function windChill(tempF, speed) {
+	if ((tempF > 50) || (speed < 3)) {
+		let result = "N/A";
+		return result ;
+	}
+	
+	else {
+		let result = 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 * tempF * Math.pow(speed, 0.16);
+		return result.toFixed(2) + " F&deg";
+	}
+}
