f79927aeb9e9aae40d9aa0fda0b908a331ae6fac
lucilacardus <lucila.cardus@gmail.com>
2019-07-20 15:58:32 -0300

    DFGDGD

--- js/w13.1.js
+++ js/w13.1.js
@@ -1,40 +1,20 @@
-/*input: Create a random array
-Processing: get values from the random array and gets the middle of it
-Output: Displays the random array and the middle of it*/
-function createArray() {
-	let array = [];
+/*Input: Array with integers
+Processing: A function named countEvens counts and returns the number of even integers in the array. 
+Output: Display amount of even integers
+*/
 
-	let arrayList = "";
+function programTester() {
+	let numbers = [25, 12, 28, 258, 81, 1, 3];
+	document.getElementById("array").innerHTML = numbers.toString();
+	document.getElementById("output").innerHTML = countEvens(numbers);
+}
 
-	let size = Math.floor(Math.random() * 20 + 1);
-
-	for (let i = 0; i < size; i++) {
-		array[i] = Math.floor(Math.random() * 100 + 1);
-
-		if (i != size - 1) {
-			arrayList += array[i] + ", ";
-		} else {
-			arrayList += array[i];
+function countEvens(list) {
+	let evens = 0;
+	for (i = 0; i < list.length; i++) {
+		if (list[i] % 2 == 0) {
+			evens++;
 		}
 	}
-
-	document.getElementById("arrayList").innerHTML = "Array: [" + arrayList + "]";
-
-	let middle = getMiddle(array);
-
-	document.getElementById("result").innerHTML =
-		"The Middle Element of the array is: " + middle;
-}
-function getMiddle(list) {
-	let result;
-
-	if (list.length % 2 == 0) {
-		let index = list.length / 2 - 1;
-		result = (list[index] + list[index + 1]) / 2;
-	} else {
-		let index = parseInt(list.length / 2);
-		result = list[index];
-	}
-
-	return result;
+	return evens;
 }
