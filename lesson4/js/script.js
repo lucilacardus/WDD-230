bb6f0f264cfaacefd916e42e8db8083e48e5190f
lucilacardus <lucila.cardus@gmail.com>
2020-05-16 23:34:37 -0300

    hgjhgj

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,1 +1,6 @@
 document.getElementById('last').textContent = document.lastModified;
+
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
