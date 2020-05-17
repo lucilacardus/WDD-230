53a17790806653d82450d770225a88bdcb40d2bb
lucilacardus <lucila.cardus@gmail.com>
2020-05-16 23:58:47 -0300

    xcxc

--- lesson4/js/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('last').textContent = document.lastModified;
-
-const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.navigation')
-
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
