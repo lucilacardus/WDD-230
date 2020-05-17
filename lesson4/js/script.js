e0e1bf73f158ee16226d6472917f3a05403609b6
lucilacardus <lucila.cardus@gmail.com>
2020-05-17 01:25:54 -0300

    ggfgd

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,15 +1,5 @@
+function toggleMenu() {
+    document.getElementsByClassName("navigation") [0] .classList.toggle("responsive");
 
-const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.menu');
-
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
-
-const options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric'
-  };
-  
-  let date = new Date();
-  let local = date.toLocaleDateString('en-US', options);
+    
+}
