317287489443aea7d656346575a948e89c73d9f6
lucilacardus <lucila.cardus@gmail.com>
2020-05-20 20:45:02 -0300

    vcvcv

---
+++ lesson5/js/script.js
@@ -0,0 +1,36 @@
+function toggleMenu() {
+    document.getElementsByClassName("navigation") [0] .classList.toggle("responsive");
+
+    
+}
+
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+let date = new Date();
+let dayName = daynames[date.getDay()];
+let monthName = months[date.getMonth()];
+let fulldate = dayName + ", " + date.getDate() + " " + monthName + " " + date.getFullYear();
+
+document.getElementById("currentdate").textContent = fulldate;
