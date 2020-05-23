262e2b5bfc64645b5679881e22fea8c7e8d8a3b7
lucilacardus <lucila.cardus@gmail.com>
2020-05-23 15:51:06 -0300

    dfgdfgdf

--- lesson5/js/banner.js
+++ lesson5/js/banner.js
@@ -1,15 +1,9 @@
 
-document.getElementById("year").textContent = new Date().getFullYear();
-var d = new Date();
-var day = d.getDate();
-const WeekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-document.getElementById("currentTime").innerHTML = WeekDay[d.getDay()] + ", " + (new Date().toLocaleString
-("en",{month:'long', year:'numeric', day:'numeric'})) + " " + new Date(new Date().getTime()).toLocaleTimeString();
 
-const banner = document.querySelector('#banner');
+const banner = document.getElementById('banner');
 
 
-if(WeekDay[d.getDay()] == "Friday") {
+if(d.getDay()] == 5) {
     banner.style.display = 'block';
 }
 else {
