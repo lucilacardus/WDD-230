5895daa8c29f7565db580da00eb9064a1e152ae9
lucilacardus <lucila.cardus@gmail.com>
2020-05-23 15:06:21 -0300

    cvcvvc

--- lesson5/js/banner.js
+++ lesson5/js/banner.js
@@ -1,4 +1,12 @@
 
+document.getElementById("year").textContent = new Date().getFullYear();
+var d = new Date();
+var day = d.getDate();
+const WeekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+document.getElementById("currentTime").innerHTML = WeekDay[d.getDay()] + ", " + (new Date().toLocaleString
+("en",{month:'long', year:'numeric', day:'numeric'})) + " " + new Date(new Date().getTime()).toLocaleTimeString();
+
+
 if(WeekDay[d.getDay()] == "Friday") {
     document.getElementById("banner").style.display = 'block';
 }
--- lesson5/js/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("year").textContent = new Date().getFullYear();
-var d = new Date();
-var day = d.getDate();
-const WeekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-document.getElementById("currentTime").innerHTML = WeekDay[d.getDay()] + ", " + (new Date().toLocaleString
-("en",{month:'long', year:'numeric', day:'numeric'})) + " " + new Date(new Date().getTime()).toLocaleTimeString();
