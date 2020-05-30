e98f0af1153d5460bc451012378ca573c660c079
lucilacardus <lucila.cardus@gmail.com>
2020-05-30 01:08:03 -0300

    bvbv

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -9,12 +9,12 @@ function windchill(){
 function windc(temp,speed){
     if
     (temp >= 50 && speed > 3){
-    let f = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed,0.16) + 0.4275 * temp * Math.pow(speed,0.16);
-     return Math.round(f * 10)/10; 
+    let x = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed,0.16) + 0.4275 * temp * Math.pow(speed,0.16);
+     return Math.round(x * 10)/10; 
     } 
     else{
-        let f = "n/a";
-        return f;
+        let x = "n/a";
+        return x;
     }
 }
 windchill()
