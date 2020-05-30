0a2dc69012cd8ec2f538c70f9cb28cd8074cb3d4
lucilacardus <lucila.cardus@gmail.com>
2020-05-30 00:26:18 -0300

    vcvc

---
+++ lesson6/js/windchill.js
@@ -0,0 +1,20 @@
+function windchill(){
+    let temp = parseFloat(document.getElementById("temperature").textContent);
+    let wind = parseFloat(document.getElementById("wind").textContent);
+    let result = windChill(temperature,wind);
+    
+    document.getElementById("cwind").innerHTML = result;
+    
+}
+function wind(temp,speed){
+    if
+    (temp >= 50 && speed > 3){
+    let f = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed,0.16) + 0.4275 * temp * Math.pow(speed,0.16);
+     return Math.round(f * 10)/10; 
+    } 
+    else{
+        let f = "n/a";
+        return f;
+    }
+}
+windchill()
