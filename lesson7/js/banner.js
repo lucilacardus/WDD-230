b3584cedc3198817660b426c7b18b62f9f53b951
lucilacardus <lucila.cardus@gmail.com>
2020-06-07 01:05:30 -0300

    fdfd

---
+++ lesson7/js/banner.js
@@ -0,0 +1,11 @@
+let d = new Date();
+
+const banner = document.getElementById('banner');
+
+
+if(d.getDay() == 5) {
+    banner.style.display = 'block';
+}
+else {
+    banner.style.display = 'none';
+}
