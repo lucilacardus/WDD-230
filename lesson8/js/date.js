870c6ddcfc54394938bc8a04d1d8482f35bb13e8
lucilacardus <lucila.cardus@gmail.com>
2020-06-12 23:22:49 -0300

    vcvc

--- lesson8/js/date.js
+++ lesson8/js/date.js
@@ -4,29 +4,8 @@ function toggleMenu() {
     
 }
 
-let daynames = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-];
-let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-];
+let daynames = [ "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+let months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
 let date = new Date();
 let dayName = daynames[date.getDay()];
