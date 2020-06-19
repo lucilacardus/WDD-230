27ef0e5784705fc11f1390371a8bcbb906c0e8c5
lucilacardus <lucila.cardus@gmail.com>
2020-06-18 23:24:39 -0300

    gg

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -1,33 +1,31 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 
 fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-      const prophets = jsonObject['prophets'];
-       
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+            console.table(jsonObject); 
+            const prophets = jsonObject['prophets'];
+            for (let i = 0; i < prophets.length; i++) {
+                let card = document.createElement('section');
+                let h2 = document.createElement('h2');
+                let p = document.createElement('p');
+                let p2 = document.createElement('p');
+                let image = document.createElement('img');
 
+
+                h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+                p.textContent = "Date of Birth: " +' ' + prophets[i].birthdate;
+                p2.textContent = "Place of Birth: " + prophets[i].birthplace;
+                image.setAttribute('src', prophets[i].imageurl);
+                image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' ' + prophets[i].order);
+            
+                card.appendChild(h2);
+                card.appendChild(p);
+                card.appendChild(p2);
+                card.appendChild(image); 
+
+                document.querySelector('div.cards').appendChild(card);
+            }});
         
-      forEach(prophet => {
-        let card = document.createElement('section');
-        let h2 = document.createElement('h2');
-        let p1 = document.createElement('p');
-        let p2 = document.createElement('p');
-        let image = document.createElement('img');
-        
-        h2.innerHTML = `${prophet.name} ${prophet.lastname}`;
-        p1.innerHTML = `Date of birth: <strong>${prophet.birthdate}</strong>`;
-        p2.innerHTML = `Place of birth: <strong>${prophet.birthplace}</strong>`;
-        image.innerHTML = `${prophet.imageurl}`;
-
-
-        card.appendChild(h2);
-        card.appendChild(p1);
-        card.appendChild(image);
-
-        document.querySelector('div.cards').appendChild(card);
-
-      }
-
-      )})
