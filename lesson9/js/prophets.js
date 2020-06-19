c18c4a514fae2bc4199732b7d65ac750037934bb
lucilacardus <lucila.cardus@gmail.com>
2020-06-18 22:57:04 -0300

    ghg

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -5,19 +5,27 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-  });
+      const prophets = jsonObject['prophets'];
+       {
 
-  const prophets = jsonObject['prophets'];
+        
+      forEach(prophet => {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let p1 = document.createElement('p');
+        let p2 = document.createElement('p');
+        let image = document.createElement('img');
+        
+        h2.innerHTML = `${prophet.name} ${prophet.lastname}`;
+        p1.innerHTML = `Date of birth: <strong>${prophet.birthdate}</strong>`;
+        p2.innerHTML = `Place of birth: <strong>${prophet.birthplace}</strong>`;
+        image.innerHTML = `${prophet.imageurl}`;
 
-  for (let i = 0; i < prophets.length; i++ ) {
 
+        card.appendChild(h2);
+        card.appendChild(p1);
+        card.appendChild(image);
 
-let card = document.createElement('section');
-let h2 = document.createElement('h2');
+        document.querySelector('div.cards').appendChild(card);
 
-h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-
-card.appendChild(h2);
-
-document.querySelector('div.cards').appendChild(card);
+      });
