53c7cff6e2e8d645d1a62946318441e7333c2bef
lucilacardus <lucila.cardus@gmail.com>
2020-06-19 23:10:40 -0300

    ff

--- lesson9/js/content.js
+++ lesson9/js/content.js
@@ -14,30 +14,32 @@ fetch(requestURL)
             console.table(jsonObject); 
             const towns = jsonObject['towns'];
             for (let i = 0; i < towns.length; i++) {
-                let card = document.createElement('section');
+                let town = document.createElement('section');
                 let h2 = document.createElement('h2');
                 let h3 = document.createElement('h3');
                 let p = document.createElement('p');
                 let p2 = document.createElement('p');
                 let p3 = document.createElement('p');
                 let image = document.createElement('img');
-
+                
+                
 
                 h2.textContent = towns[i].name;
                 h3.textContent = towns[i].motto;
                 p.textContent = "Year Founded: " +' ' + towns[i].yearFounded;
                 p2.textContent = "Population: " + towns[i].currentPopulation;
                 p3.textContent = "Annual Rain Fall: " + towns[i].averageRainfall;
-                image.setAttribute('src', towns[i].photo);
-                image.setAttribute('alt', towns[i].name + ' ' + towns[i].order);
-            
-                card.appendChild(h2);
-                card.appendChild(h3);
-                card.appendChild(p);
-                card.appendChild(p2);
-                card.appendChild(p3);
-                card.appendChild(image); 
+                image.setAttribute('src', "images/" + cities[i].photo);
 
-                document.querySelector('div.home').appendChild(card);
+
+                town.appendChild(h2);
+                town.appendChild(h3);
+                town.appendChild(p);
+                town.appendChild(p2);
+                town.appendChild(p3);
+                town.appendChild(image); 
+                town.appendChild(image)
+
+                document.querySelector('div.home').appendChild(town);
             }});
         
