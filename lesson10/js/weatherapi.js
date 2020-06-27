83c1426052f2e2e647c44b279f04c7fad4e69959
lucilacardus <lucila.cardus@gmail.com>
2020-06-26 22:06:20 -0300

    nh

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,8 @@
+const apiURL = 'https://api.openweather.org.data/2.5/weather?id=5604473&APPID=759aa1ea97529d151a0df861e831f226&units=imperial';
+
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+  });
