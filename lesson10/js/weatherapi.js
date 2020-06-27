483c086d773f16480cd9d77ff2cfb8cff35be111
lucilacardus <lucila.cardus@gmail.com>
2020-06-26 22:23:03 -0300

    f

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -5,4 +5,14 @@ fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
+    const currentTemp = document.querySelector ('#current-temp');
+
+    const icon = document.querySelector('img');
+    currentTemp.textContent = jsObject.main.temp;
+    const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+
+    icon.setAttribute('src', imagesrc);
+    icon.setAttribute('alt', desc);
+    
   });
