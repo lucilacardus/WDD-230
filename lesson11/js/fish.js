0ecb598e2c7e58a72abb2ac89aa941ac5503a02d
lucilacardus <lucila.cardus@gmail.com>
2020-07-04 19:52:06 -0300

    dd

---
+++ lesson11/js/fish.js
@@ -0,0 +1,16 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const city = jsonObject['towns'];
+    const preston = city.filter(x => x.name.includes("Fish Haven"));
+    const events = preston[0].events;
+
+    events.forEach(event => {
+        let info = document.createElement('p');
+        info.innerText = event             
+        document.querySelector('div.events').appendChild(info);
+    })})
