7d98cc8681974b32e4d0432374c002504354c7c7
lucilacardus <lucila.cardus@gmail.com>
2020-07-20 01:03:33 -0300

    hvf

--- finalproject/js/data.js
+++ finalproject/js/data.js
@@ -1,20 +1,20 @@
-fetch("https://lucilacardus.github.io/finalproject/data/rentals.json")
-.then(res=>{
-    res.json().then(
-        data=>{
-            console.log(data);
-            if(data.length > 0){
-                let temp ="";
+const requestURL = 'https://lucilacardus.github.io/finalproject/data/rentals.json';
 
-                data.foreach((rentals)=>{
-                    temp+= '<tr>';
-                    temp+= '<td> ' + rentals[i].name + '</td> ';
-                    temp+= '<td> ' + rentals[i].maxpersons; + '</td> ';
-                    temp+= '<td> ' + rentals[i].half; + '</td> ';
-                    temp+= '<td> ' + rentals[i].full; + '</td> ';
-                    temp+= '<td> ' + rentals[i].halfwalkin; + '</td> ';
-                    temp+= '<td> ' + rentals[i].fullwalkin; + '</td> </tr>';
-                })
-                document.getElementById("data").innerHTML = temp;
-            }}
-    )})
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+            console.table(jsonObject); 
+            const rentals = jsonObject['rentals'];
+            let temp ="";
+                temp+= '<tr>';
+                temp+= '<td> ' + rentals[i].name + '</td> ';
+                temp+= '<td> ' + rentals[i].maxpersons; + '</td> ';
+                temp+= '<td> ' + rentals[i].half; + '</td> ';
+                temp+= '<td> ' + rentals[i].full; + '</td> ';
+                temp+= '<td> ' + rentals[i].halfwalkin; + '</td> ';
+                temp+= '<td> ' + rentals[i].fullwalkin; + '</td> </tr>';
+            
+            document.getElementById("data").innerHTML = temp
+        });
