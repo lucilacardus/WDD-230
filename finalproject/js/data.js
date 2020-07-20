4afc2776c25fb5a7e62d24062270d6fa35e885aa
lucilacardus <lucila.cardus@gmail.com>
2020-07-20 13:39:35 -0300

    xx

--- finalproject/js/data.js
+++ finalproject/js/data.js
@@ -8,13 +8,13 @@ fetch(requestURL)
         console.table(jsonObject); 
         const rentals = jsonObject['rentals'];
         for (let i = 0; i < rentals.length; i++) {
-            let temp = document.createElement('tr');
+            let tr = document.createElement('tr');
 
                 
             tr.textContent =`<td>${rentals[i].name}</td> <td> ${rentals[i].maxpersons}</td> <td> ${rentals[i].half}</td> <td> ${rentals[i].full}</td><td>${rentals[i].halfwalkin}</td><td>${rentals[i].fullwalkin}</td>`;
-            containtext.appendChild(temp);
+            containtext.appendChild(tr);
    
-            document.querySelector('div.data').appendChild(temp);    
+            document.querySelector('div.data').appendChild(tr);    
             
             
     }});
