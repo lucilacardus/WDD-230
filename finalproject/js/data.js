2307f697d14ce9d9827d3dfbf89297b20fb7c7b5
lucilacardus <lucila.cardus@gmail.com>
2020-07-20 16:12:24 -0300

    ff

--- finalproject/js/data.js
+++ finalproject/js/data.js
@@ -2,52 +2,50 @@ const requestURL = 'https://lucilacardus.github.io/finalproject/data/rentals.jso
 
 
 fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (jsonObject) {
-        const rentals = jsonObject['rentals'];
+.then(function(response) {
+  return response.json();
+})
+.then(function(jsonObject){
+//console.table(jsonObject);
+  const rentals = jsonObject['rentals'];
 
-        for (let i = 0; i < towns.length; i++) {
-            let place = rentals[i]
-            {
+  rentals.forEach(rentals => {
+    let content = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let h3 = document.createElement('h3');
+    let h4 = document.createElement('h3');
+    let p1 = document.createElement('p');
+    let p2 = document.createElement('p');
+    let p3 = document.createElement('p');
+    let p4 = document.createElement('p');
+    let p5 = document.createElement('p');
+    let image = document.createElement('img');
 
-                let section = document.createElement('article');
-                section.classList.add('maincontainer')
+    image.setAttribute('src', 'images/' + rentals.picture);
+    image.setAttribute('alt', `${rentals.name}`);
+    h2.innerHTML = `${rentals.name}`;
+    p1.innerHTML = `Max. Persons: ${rentals.maxpersons}`;
+    h3.innerHTML = `Reservation`;
+    p2.innerHTML = `Half Day (3 hrs): ${rentals.half}`;
+    p3.innerHTML = `Full Day: ${rentals.full}`;
+    h4.innerHTML = `Walk-In`;
+    p4.innerHTML = `Half Day (3 hrs): ${rentals.halfwalkin}`;
+    p5.innerHTML = `Full Day: ${rentals.fullwalkin}`;
+    
 
-                let containtext = document.createElement('div');
-                containtext.classList.add('textcol');
-                let h2 = document.createElement('h2');
-                let motto = document.createElement('h3');
-                let year = document.createElement('p');
-                let population = document.createElement('p');
-                let rainfall = document.createElement('p');
-
-                h2.textContent = place.name;
-                motto.textContent = place.motto;
-                year.textContent = `Year Founded: ${place.yearFounded}`;
-                population.textContent = 'Population: ' + place.currentPopulation;
-                rainfall.textContent = 'Annual Rain Fall: ' + place.averageRainfall;
-
-                containtext.appendChild(h2);
-                containtext.appendChild(motto);
-                containtext.appendChild(year);
-                containtext.appendChild(population);
-                containtext.appendChild(rainfall);
-
-                let picture = document.createElement('div');
-                picture.classList.add('homepic')
-                let image = document.createElement('img');
-                
-                image.setAttribute('src', "images/" + place.photo);
-                picture.appendChild(image)
-                image.setAttribute('alt', towns[i].name);
-
-                section.appendChild(containtext)
-                section.appendChild(picture)
+    content.appendChild(image);
+    content.appendChild(h2);
+    content.appendChild(p1);
+    content.appendChild(h3);
+    content.appendChild(p2);
+    content.appendChild(p3);
+    content.appendChild(h4);
+    content.appendChild(p4);
+    content.appendChild(p5);
+    
 
 
-                document.querySelector('div.home').appendChild(section);
-            }
-        }
-    });
+    document.querySelector('div.data').appendChild(content);
+    
+  });
+});
