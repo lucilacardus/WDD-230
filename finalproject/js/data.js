4d912f273bcd58f48f5daf2d69d6b53b53809133
lucilacardus <lucila.cardus@gmail.com>
2020-07-19 23:31:08 -0300

    tt

--- finalproject/js/data.js
+++ finalproject/js/data.js
@@ -11,7 +11,7 @@ fetch(rentalurl)
            
 
 
-            tr.textContent = '<td> ' + rentals[i].name + '</td> ' + '<td> ' + rentals[i].maxpersons; + '</td> ' + '<td> ' + rentals[i].half + '</td> ' + '<td> ' + rentals[i].full + '</td> '+ '<td> ' + rentals[i].halfwalkin; + '</td> '+ '<td> ' + rentals[i].fullwalkin; + '</td> ';
+            tr.textContent = `<td> ${rentals[i].name}</td> <td> ${rentals[i].maxpersons}`; + '</td> ' + '<td> ' + rentals[i].half + '</td> ' + '<td> ' + rentals[i].full + '</td> '+ '<td> ' + rentals[i].halfwalkin; + '</td> '+ '<td> ' + rentals[i].fullwalkin; + '</td> ';
            
             
         
