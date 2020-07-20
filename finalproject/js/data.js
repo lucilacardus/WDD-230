0b2a79c82b9ea63484d3205de2ffd899475028c7
lucilacardus <lucila.cardus@gmail.com>
2020-07-20 00:18:42 -0300

    vgdfbgd

--- finalproject/js/data.js
+++ finalproject/js/data.js
@@ -8,16 +8,19 @@ fetch(rentalurl)
         const rentals = jsonObject['rentals'];
         for (let i = 0; i < rentals.length; i++) {
 
+            let card = document.createElement('section');
+
             let tr = document.createElement('tr');
            
 
 
             tr.textContent = '<td> ' + rentals[i].name + '</td> ' + '<td> ' + rentals[i].maxpersons; + '</td> ' + '<td> ' + rentals[i].half + '</td> ' + '<td> ' + rentals[i].full + '</td> '+ '<td> ' + rentals[i].halfwalkin; + '</td> '+ '<td> ' + rentals[i].fullwalkin; + '</td> ';
-           
+            card.appendChild(tr);
+
             
         
             
 
-            document.querySelector('div.datatable');tr;
+            document.querySelector('div.datatable').appendChild(card);
         }});
     
