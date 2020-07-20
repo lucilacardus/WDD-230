c0ce45ba48e4c9bc7ade5f7641bba41950fb80f8
lucilacardus <lucila.cardus@gmail.com>
2020-07-20 14:32:50 -0300

    f

--- finalproject/js/data.js
+++ finalproject/js/data.js
@@ -13,7 +13,7 @@ fetch(requestURL)
                 tr.classList.add('datatable')
 
                 
-                let tr = document.createElement('tr');
+                
                 
 
                 tr.innerHTML =`<td>${rentals[i].name}</td> <td> ${rentals[i].maxpersons}</td> <td> ${rentals[i].half}</td> <td> ${rentals[i].full}</td><td>${rentals[i].halfwalkin}</td><td>${rentals[i].fullwalkin}</td>`;
