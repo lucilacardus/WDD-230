bd984b18d66d2960b88125914cfe4d2b7236f0d6
lucilacardus <lucila.cardus@gmail.com>
2020-07-20 13:32:46 -0300

    dd

--- finalproject/js/data.js
+++ finalproject/js/data.js
@@ -11,9 +11,10 @@ fetch(requestURL)
             let temp = document.createElement('tr');
 
                 
-            temp =`<td>${rentals[i].name}</td> <td> ${rentals[i].maxpersons}</td> <td> ${rentals[i].half}</td> <td> ${rentals[i].full}</td><td>${rentals[i].halfwalkin}</td><td>${rentals[i].fullwalkin}</td>`;
-               
-                
+            temp.textContent =`<td>${rentals[i].name}</td> <td> ${rentals[i].maxpersons}</td> <td> ${rentals[i].half}</td> <td> ${rentals[i].full}</td><td>${rentals[i].halfwalkin}</td><td>${rentals[i].fullwalkin}</td>`;
+            containtext.appendChild(temp);
+   
+            document.querySelector('tr.data').appendChild(temp);    
             
-            document.getElementById("data").innerHTML = temp
+            
     }});
